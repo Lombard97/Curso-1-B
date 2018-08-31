425c971983de2cd6db8ee492396eff7a70b37be3
Lombard97 <lombard_k-po@hotmail.com>
2018-08-31 12:07:37 -0300

    funciones en bibliotecas
    bibliotecas documentadas al estilo doxygen

--- Ejercicio Funciones/main.c
+++ Ejercicio Funciones/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "UTN_INPUT.h"
 
-int pedirNum();
-float pedirFlotante();
-char pedirCaracter();
+int pedirNum(char[],int,int);
+float pedirFlotante(char[]);
+char pedirCaracter(char[]);
+int validarEntero(int,char[],int,int);
+
 
 int main()
 {
@@ -11,37 +14,14 @@ int main()
     float nota;
     char genero;
 
-    legajo=pedirNum();
-    printf("El numero de legajo es %d\n",legajo);
+    legajo=pedirNum(" edad\n",18,65);
+    printf("la edad es %d\n",legajo);
 
-    nota=pedirFlotante();
+    nota=pedirFlotante("ingrese la nota\n");
     printf("La nota ingresa es %.2f\n",nota);
 
-    genero=pedirCaracter();
+    genero=pedirCaracter("ingrese su genero\n");
     printf("El genero ingresado es %c",genero);
 
-}
-
-int pedirNum()
-{
-    int legajo;
-    printf("Ingrese Legajo\n");
-    scanf("%d",&legajo);
-    return legajo;
-}
-float pedirFlotante()
-{
-    float nota;
-    printf("Ingrese nota");
-    scanf("%f",&nota);
-    return nota;
-}
-char pedirCaracter()
-{
-    char genero;
-    printf("Ingrese M/F segun su genero");
-    fflush(stdin);
-    scanf("%c",&genero);
-    return genero;
 }
 
