94c8d683dddfacbd0611de5de58e95cba2a00c3b
Lombard97 <lombard_k-po@hotmail.com>
2018-11-02 10:56:45 -0300

    tp3 (terminar)

---
+++ TP3/Win_32/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    FILE* pData;
+    pData = fopen("Data.csv","r");
+    int cantidad;
+    int error;
+    int option = 0;
+    int i;
+    /*printf("OPCIONES\n1-Cargar datos desde el archivo\n2-Cargar datos desde el archivo (modo binario)\n3-Dar de alta un empleado\n4-Salir");
+    scanf("%d",&option);*/
+
+    LinkedList* listaEmpleados = ll_newLinkedList();
+
+    /*error = parser_EmployeeFromBinary(pData,listaEmpleados);*/
+
+    cantidad = fscanf(pData,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",;
+
+    printf("%d",cantidad);
+
+
+    do{
+        switch(option)
+        {
+            case 1:
+                controller_loadFromText("data.csv",listaEmpleados);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }while(option != 4);
+
+    return 0;
+}
---
+++ TP3/Win_32/parser.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    pFile = fopen("data.csv","r");
+    int error=0;
+    int cantidad;
+    if(pFile==NULL);
+    {
+        error=-1;
+    }
+    while(!feof(pFile));
+    {
+       cantidad = fscanf(pFile,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",pArrayListEmployee);
+
+       printf("%d",cantidad);
+    }
+
+    return 1;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
